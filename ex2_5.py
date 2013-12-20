ed725c389837184ee1fc51f613a76f7b17ab1aa2
Iain Sproat <iainsproat@gmail.com>
2013-12-20 22:17:53 +0000

    exercise 2.5

---
+++ ex2_5.py
@@ -0,0 +1,23 @@
+﻿import Pmf
+
+def PmfMean(t):
+    mean = 0
+    for k, v in t.d.items():
+        mean += v * k
+    return mean
+
+def PmfVariance(t, mean=None):
+    if mean == None:
+        mean = PmfMean(t)
+    
+    variance = 0
+    for k, v in t.d.items():
+        variance += v * (k - mean)**2
+    
+    return variance
+
+pmf = Pmf.MakePmfFromList([1, 2, 2, 3, 5])
+print ('PmfMean : ', PmfMean(pmf))
+print ('Mean : ', pmf.Mean())
+print ('PmfVariance : ', PmfVariance(pmf))
+print ('Variance : ', pmf.Var())
