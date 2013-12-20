76a68fc09964224e449632a17b4028251a90a4eb
Iain Sproat <iainsproat@gmail.com>
2013-12-20 16:56:23 +0000

    Refactor ex1_3
    allow functions to be used by ex2_2

--- ex1_3.py
+++ ex1_3.py
@@ -2,52 +2,73 @@
 import sys
 from datetime import timedelta
 
-def isLiveBirth(self):
+def IsLiveBirth(self):
     return self.outcome == 1
 
-def isFirstBorn(self):
+def IsFirstBorn(self):
     return self.birthord == 1
 
+def MonkeyPatch():
+    global IsLiveBirth
+    global IsFirstBorn
+    survey.Pregnancy.IsLiveBirth = IsLiveBirth
+    survey.Pregnancy.IsFirstBorn = IsFirstBorn
+
+def CleanupMonkeyPatching():
+    global IsLiveBirth
+    global IsFirstBorn
+    del(IsLiveBirth)
+    del(IsFirstBorn)
+
+def PartitionFirstBornFromSubsequentBorn(records):
+    MonkeyPatch()
+    firstBorn = []
+    subsequentBorn = []
+    for record in records:
+        if not record.IsLiveBirth():
+            continue
+            
+        if record.IsFirstBorn():
+            firstBorn.append(record)
+        else:
+            subsequentBorn.append(record)
+                
+    CleanupMonkeyPatching()
+    return firstBorn, subsequentBorn
+
+def SumPregnancyLength(records):
+    sumPrgLengths = 0
+    for record in records:
+        sumPrgLengths += record.prglength
+    return sumPrgLengths
+
 def main(name):
-    #monkey patch
-    global isLiveBirth
-    global isFirstBorn
-    survey.Pregnancy.isLiveBirth = isLiveBirth
-    survey.Pregnancy.isFirstBorn = isFirstBorn
 
     table = survey.Pregnancies()
     table.ReadRecords()
     print ('Number of pregnancies', len(table.records))
-    liveBirths = 0
-    firstBorn = 0
-    notFirstBorn = 0
-    sumFirstBornPrgLength = 0
-    sumNotFirstBornPrgLength = 0
 
-    for rec in table.records:
-        if rec.isLiveBirth():
-            liveBirths += 1
-            if rec.isFirstBorn():
-                firstBorn += 1
-                sumFirstBornPrgLength += rec.prglength
-            else:
-                notFirstBorn += 1
-                sumNotFirstBornPrgLength += rec.prglength
+    firstBorn, subsequentBorn = PartitionFirstBornFromSubsequentBorn(table.records)
+    
+    numberFirstBorn = len(firstBorn)
+    numberSubsequentBorn = len(subsequentBorn)
+    numberLiveBirths = numberFirstBorn + numberSubsequentBorn
+    
+    sumFirstBornPrgLength = SumPregnancyLength(firstBorn)
+    sumSubsequentBornPrgLength = SumPregnancyLength(subsequentBorn)
 
-    avgFirstBornPrgLength = timedelta(weeks = (sumFirstBornPrgLength / firstBorn))
-    avgNotFirstBornPrgLength = timedelta(weeks = (sumNotFirstBornPrgLength / notFirstBorn))
+    avgFirstBornPrgLength = timedelta(weeks = (sumFirstBornPrgLength / numberFirstBorn))
+    avgSubsequentBornPrgLength = timedelta(weeks = (sumSubsequentBornPrgLength / numberSubsequentBorn))
 
-    diff = avgFirstBornPrgLength - avgNotFirstBornPrgLength
+    diffFirstBornToSubsequentBornPrgLength = avgFirstBornPrgLength - avgSubsequentBornPrgLength
 
-    print ('Number of live births', liveBirths)
-    print ('Number of live first born', firstBorn)
-    print ('Number of live second or later born', notFirstBorn)
-    print ('Average pregnancy length of first born', avgFirstBornPrgLength)
-    print ('Average pregnancy length of second or later born', avgNotFirstBornPrgLength)
-    print ('Difference in pregnancy length between first born and later born', diff)
+    print ('Number of live births:               ', numberLiveBirths)
+    print ('Number of live first born:           ', numberFirstBorn)
+    print ('Number of live second or later born: ', numberSubsequentBorn)
+    print ('Average pregnancy length of first born:           ', avgFirstBornPrgLength)
+    print ('Average pregnancy length of second or later born: ', avgSubsequentBornPrgLength)
+    print ('Difference in pregnancy length between first born and later born: ', diffFirstBornToSubsequentBornPrgLength)
 
-    del(isLiveBirth)
-    del(isFirstBorn)
 
 if __name__ == '__main__':
     main(*sys.argv)
